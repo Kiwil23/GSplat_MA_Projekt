641316a02349486d5e93d89cea69e06c35f04505
Kiwi23 | Steffen Rohe <100080346+Kiwil23@users.noreply.github.com>
2025-05-03 16:40:41 +0200

    Updated Pipeline (now modular) and readme

--- pipeline_assets/jobscripts/Mockup.sh
+++ pipeline_assets/jobscripts/Mockup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 docker run -it --gpus all \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/input_data:/mnt/input_data \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/result_data:/mnt/result_data \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/scripts:/mnt/pipeline_scripts \
+  -v <PATH_TO>input_data:/mnt/input_data \
+  -v <PATH_TO>result_data:/mnt/result_data \
+  -v <PATH_TO>scripts:/mnt/pipeline_scripts \
   -p 7007:7007  \
   splat_tools_slim \
   python3 /mnt/pipeline_scripts/pipeline.py \
   --pipeline_type="mp4_to_splat" \
-  --is_big_dataset="True" && echo "pipeline.py done"
+  --is_big_dataset="False" && echo "pipeline.py done"
 
 
