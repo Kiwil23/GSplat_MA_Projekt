67d9f0d80d523f443d1ab6a6c7c664bae7523b75
Kiwi23 | Steffen Rohe <100080346+Kiwil23@users.noreply.github.com>
2025-05-04 19:58:41 +0200

    Update Mockup.sh

--- pipeline_assets/jobscripts/Mockup.sh
+++ pipeline_assets/jobscripts/Mockup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 docker run -it --gpus all \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/input_data:/mnt/input_data \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/result_data:/mnt/result_data \
-  -v /mnt/c/Users/rohes/Desktop/PipelineTest/scripts:/mnt/pipeline_scripts \
+  -v <PATH_TO>/input_data:/mnt/input_data \
+  -v <PATH_TO>/result_data:/mnt/result_data \
+  -v <PATH_TO>/scripts:/mnt/pipeline_scripts \
   -p 7007:7007  \
   splat_tools_slim \
   python3 /mnt/pipeline_scripts/pipeline.py \
